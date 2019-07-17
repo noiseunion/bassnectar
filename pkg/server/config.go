d94f06a740eecdad4f9adfc22b81d8afd6eb1478
JD Hendrickson <jd@randomworx.com>
2019-07-17 16:09:44 -0600

    Create config.go

---
+++ pkg/server/config.go
@@ -0,0 +1,12 @@
+package server
+
+import "github.com/go-chi/cors"
+
+// Config for the server
+type Config struct {
+	Addr         string `config:"addr"`
+	Port         string `config:"port"`
+	RouteBuilder *RouteBuilder
+	CORS         cors.Options
+	DisableCORS  bool
+}
