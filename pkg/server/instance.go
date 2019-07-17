3e42c66c55eefdb8fbb7c99d3bbf7f7cfacc2b0b
JD Hendrickson <jd@randomworx.com>
2019-07-17 16:09:30 -0600

    Create instance.go

---
+++ pkg/server/instance.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/sirupsen/logrus"
+)
+
+// Instance is an "instance" of our server
+type Instance struct {
+	Logger       *logrus.Logger
+	httpServer   *http.Server
+	router       *chi.Mux
+	routeBuilder *RouteBuilder
+}
+
+// RenderRoutes will render our RouteBuilder routes into the server instance.
+func (instance *Instance) RenderRoutes() {
+	rb := instance.routeBuilder
+
+	for _, route := range rb.Routes {
+		switch route.Method {
+		case "GET":
+			instance.router.Get(route.Path, route.Handler)
+		case "POST":
+			instance.router.Post(route.Path, route.Handler)
+		case "DELETE":
+			instance.router.Delete(route.Path, route.Handler)
+		default:
+			logrus.Errorf("Unknown route method: %s for %+v", route.Method, route)
+		}
+	}
+}
