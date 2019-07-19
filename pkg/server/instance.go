e97b7a831409e772d929130b2f26f4d479675156
JD Hendrickson <jd@randomworx.com>
2019-07-19 11:03:03 -0600

    Update instance.go

--- pkg/server/instance.go
+++ pkg/server/instance.go
@@ -17,18 +17,5 @@ type Instance struct {
 
 // RenderRoutes will render our RouteBuilder routes into the server instance.
 func (instance *Instance) RenderRoutes() {
-	rb := instance.routeBuilder
-
-	for _, route := range rb.Routes {
-		switch route.Method {
-		case http.MethodGet:
-			instance.router.Get(route.Path, route.Handler)
-		case http.MethodPost:
-			instance.router.Post(route.Path, route.Handler)
-		case http.MethodDelete:
-			instance.router.Delete(route.Path, route.Handler)
-		default:
-			logrus.Errorf("Unknown route method: %s for %+v", route.Method, route)
-		}
-	}
+  instance.routeBuilder.RenderRoutes(instance)
 }
