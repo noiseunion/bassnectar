40ea19ad3a8554a08ae608c1c09b2f4bfecce70e
JD Hendrickson <jd@randomworx.com>
2019-07-19 09:10:06 -0600

    JWT Config Option
    adding config option to implement JWT authentication

--- pkg/server/config.go
+++ pkg/server/config.go
@@ -4,9 +4,10 @@ import "github.com/go-chi/cors"
 
 // Config for the server
 type Config struct {
-	Addr         string `config:"addr"`
-	Port         string `config:"port"`
-	RouteBuilder *RouteBuilder
-	CORS         cors.Options
-	DisableCORS  bool
+	Addr         	string `config:"addr"`
+	Port         	string `config:"port"`
+	RouteBuilder 	*RouteBuilder
+	CORS         	cors.Options
+	DisableCORS  	bool
+	ProtectedRoutes bool
 }
